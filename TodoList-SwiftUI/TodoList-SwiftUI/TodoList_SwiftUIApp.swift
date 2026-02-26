3239159c96d8382f15368c7b7d7b2e9c2e1198c9
Augusto Lima <augusto.lima@tracktik.com>
2026-02-26 16:07:45 -0500

    Project setup + Firebase SPM

---
+++ TodoList-SwiftUI/TodoList-SwiftUI/TodoList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_SwiftUIApp.swift
+//  TodoList-SwiftUI
+//
+//  Created by Augusto Lima on 25/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoList-SwiftUI/TodoList-SwiftUITests/TodoList_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_SwiftUITests.swift
+//  TodoList-SwiftUITests
+//
+//  Created by Augusto Lima on 25/2/2026.
+//
+
+import Testing
+@testable import TodoList_SwiftUI
+
+struct TodoList_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
